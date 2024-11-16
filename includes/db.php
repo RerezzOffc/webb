d7c790b378aebcb3eb7a9b691d41872efbe4ca03
Rerezz Official <thegamergemu0001@gmail.com>
2024-11-16 17:42:36 +0700

    Create db.php

---
+++ includes/db.php
@@ -0,0 +1,15 @@
+<?php
+// /includes/db.php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "user_management";  // Ganti dengan nama database Anda
+
+// Membuat koneksi
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Cek koneksi
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
