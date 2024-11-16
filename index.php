622c89b145e77fa18d64479208874975192ca9c6
Rerezz Official <thegamergemu0001@gmail.com>
2024-11-16 17:41:01 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+// index.php
+include('config.php');  // Memasukkan file konfigurasi
+
+// Menggunakan domain custom dari config.php
+echo BASE_URL . "login.php";  // Output: https://www.namadomain.com/login.php
+?>
